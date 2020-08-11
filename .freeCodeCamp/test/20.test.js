1f5c06c38e6e9324432d8a192848c76efc4e51da
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-11 11:31:59 -0500

    20.1

---
+++ .freeCodeCamp/test/20.test.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+const { getLastCommand, getCwd, getDirectoryContents } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand, projectDirectory;
+  before(async () => {
+    projectDirectory = await getDirectoryContents('..');
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should use "touch" to create "castle.sh"', async () => {
+    assert(projectDirectory.indexOf('castle.sh') >= 0 && lastCommand[0] === 'touch' && /castle\.sh$/.test(lastCommand[1]) && cwd === '/home/strove/project');
+  });
+});

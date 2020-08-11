72a2919a3642927e1eb8fba531ba50dfc340ad0d
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-11 12:42:05 -0500

    50.1

--- .freeCodeCamp/test/40.test.js
+++ .freeCodeCamp/test/40.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your file', () => {
@@ -7,4 +7,4 @@ describe('Your file', () => {
 
     assert(/echo hello nano/.test(castleFile));
   });
-});
+});*/

b517af8e8b3f00a97d53c43e28dbf15c60721aed
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-20 13:34:10 -0500

    240.1

---
+++ .freeCodeCamp/test/240.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('Your castle', () => {
+  let castleFile;
+  before(async () => {
+    castleFile = await getFileContents('../castle.sh');
+  });
+
+  it('should be drawn correctly', async () => {
+    assert(/^\s*echo\s+"\s*\n\/  \\ {14}\/  \\\s*\n\|  \|_{14}\|  \|\s*\n(\| {20}\|\s*\n){4}\|_{20}\|\s*\n\s*"\s*$/.test(castleFile));
+  });
+});

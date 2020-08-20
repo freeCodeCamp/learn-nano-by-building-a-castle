3be16ba2f960ac36a5bef33ae6f270c038ba625a
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-20 11:13:48 -0500

    90.1

--- .freeCodeCamp/test/80.test.js
+++ .freeCodeCamp/test/80.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your file', () => {
@@ -7,4 +7,4 @@ describe('Your file', () => {
 
     assert(!/\S/.test(castleFile));
   });
-});
+});*/

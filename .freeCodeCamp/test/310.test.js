945c9e3e8fca879a8d63144c9a11fed7892cb4e8
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-20 20:28:58 -0500

    310.1

---
+++ .freeCodeCamp/test/310.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('You', () => {
+  let cwd, lastCommand;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should use "bash" to run "castle.sh"', async () => {
+    assert(lastCommand[0] === 'bash' && /castle\.sh$/.test(lastCommand[1]) && cwd === '/home/strove/project');
+  });
+});

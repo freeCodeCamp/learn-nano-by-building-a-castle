ec61f599053bce624ceb9e55f7d4e0c265714bc5
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-20 20:45:15 -0500

    340.1

--- .freeCodeCamp/test/330.test.js
+++ .freeCodeCamp/test/330.test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+/*const assert = require('assert');
 const { getFileContents } = require('./utils');
 
 describe('Your castle', () => {
@@ -7,4 +7,4 @@ describe('Your castle', () => {
 
     assert(/echo\s+"\s*\n \/\\ {16}\/\\\s+\n\/  \\ {14}\/  \\\s+\n\|  \|_{14}\|  \|\s*\n\| {20}\|\s*\n\|   \[\] {4}\[\] {4}\[\]   \|\s*\n(\| {20}\|\s*\n){2}\|_{20}\|\s*\n"/.test(castleFile));
   });
-});
+});*/

53d771c8fb20fe398027960a3cc11e2f3971b70e
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-20 20:46:18 -0500

    350.1

---
+++ .freeCodeCamp/test/350.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getNextCommand } = require('./utils');
+
+describe('You', () => {
+  let nextCommand;
+  before(async () => {
+		nextCommand = await getNextCommand(4);
+  });
+
+  it('should exit Nano after opening "castle.sh"', async () => {
+    assert(nextCommand[0] === 'nano' && /castle\.sh$/.test(nextCommand[1]));
+  });
+});

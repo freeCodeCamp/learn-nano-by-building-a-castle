1bfcb08b71b5f11541bb12be0be741427414f723
moT01 <20648924+moT01@users.noreply.github.com>
2020-08-20 20:50:10 -0500

    380.1

---
+++ .freeCodeCamp/test/380.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const { getFileContents } = require('./utils');
+
+describe('You', () => {
+  let castleFile;
+  before(async () => {
+    castleFile = await getFileContents('../castle.sh');
+  });
+
+  it('should echo the welcome message above your castle', async () => {
+    assert(/^\s*echo\s+"\s*\n\s*Welcome to my castle\s*\n\s*"/.test(castleFile));
+  });
+});
